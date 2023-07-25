2faf459a9412faff6611ba5542a88e40c0414af3
AshRR <r.risser@ash-network.com>
2023-07-25 15:21:24 +0200

    idk2

--- grinder.py
+++ grinder.py
@@ -3,6 +3,7 @@ import os
 import time
 import tkinter as tk
 from tkinter import scrolledtext
+from sys import argv
 
 def create_ascii_window():
     root = tk.Tk()
@@ -25,15 +26,14 @@ def extract_gif_frames(gif, fillEmpty=False):
             gif.seek(gif.tell()+1)
             new_frame = Image.new('RGBA',gif.size)
             new_frame.paste(im, (0,0), im.convert('RGBA'))
-            
-            #check if we are painting over a canvas
+
             if fillEmpty:
                 canvas=Image.new('RGBA', new_frame.size, (255,255,255,255))
                 canvas.paste(new_frame, mask=new_frame)
                 new_frame=canvas
             frames.append(new_frame)
     except EOFError:
-        pass # end of sequence
+        pass
     return frames
 
 def save_frames_list(frames):        
@@ -43,9 +43,8 @@ def save_frames_list(frames):
         frame.save('test%d.png'%i,**frame.info)
 
 def convert_image_to_ascii(image):
-    font = ImageFont.load_default() # load default bitmap monospaced font
+    font = ImageFont.load_default()
     (chrx, chry) = font.getsize(chr(32))
-    # calculate weights of ASCII chars
     weights = []
     for i in range(32, 127):
         chrImage = font.getmask(chr(i))
@@ -60,14 +59,12 @@ def convert_image_to_ascii(image):
     (imgx, imgy) = image.size
     imgx = int(imgx / chrx)
     imgy = int(imgy / chry)
-    # NEAREST/BILINEAR/BICUBIC/ANTIALIAS
     image = image.resize((imgx, imgy), Image.BICUBIC)
-    image = image.convert("L") # convert to grayscale
+    image = image.convert("L")
     pixels = image.load()
     for y in range(imgy):
         for x in range(imgx):
             w = float(pixels[x, y]) / 255 / intensity_multiplier
-            # find closest weight match
             wf = -1.0; k = -1
             for i in range(len(weights)):
                 if abs(weights[i] - w) <= abs(wf - w):
@@ -85,16 +82,42 @@ def convert_frames_to_ascii(frames):
 
 def animate_ascii(ascii_frames, num_iterations):
     root, frame_text = create_ascii_window()
+
     for i in range(num_iterations):
         for frame in ascii_frames:
             update_ascii_frame(frame_text, frame)
-            time.sleep(0.05)
-            root.update()  # Update the GUI window to display the new ASCII frame
+            time.sleep(0.005)
+            root.update()
     root.destroy()
 
-im = Image.open("grinderr.gif")
+def create_ascii_images(ascii_frames):
+    font = ImageFont.truetype("cour.ttf", size=9)
+
+    ascii_images = []
+    for frame in ascii_frames:
+        img = Image.new("RGB", (500, 500), color="black")
+        draw = ImageDraw.Draw(img)
+        draw.text((9, 9), frame, fill="white", font=font)
+        ascii_images.append(img)
+
+    return ascii_images
+
+def save_ascii_as_gif(ascii_frames, output_filename):
+    ascii_images = create_ascii_images(ascii_frames)
+
+    ascii_images[0].save(
+        output_filename,
+        save_all=True,
+        append_images=ascii_images[1:],
+        duration=5,
+        loop=0
+    )
+
+im = Image.open("grinder.gif")
 frames = extract_gif_frames(im, fillEmpty=True)
 
 intensity_multiplier = 3
 ascii_frames = convert_frames_to_ascii(frames)
+
 animate_ascii(ascii_frames, num_iterations=3)
+save_ascii_as_gif(ascii_frames, "ascii_animation.gif")
